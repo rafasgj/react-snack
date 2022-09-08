7d5f5bf2d9cfa59935d1fe7956cfa828db9bcc09
Rafael Guterres Jeffman <rafasgj@gmail.com>
2022-09-07 23:22:36 -0300

    Added code for "Hello World" application.

--- App.js
+++ App.js
@@ -1,4 +1,18 @@
 
-const App = () => {}
+import {
+  View,
+  Text,
+} from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>
+        Hello World!
+      </Text>
+    </View>
+  )
+
+}
 
 export default App
